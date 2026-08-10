5cdbc296bd9c07d980ecbbbfa1d5438ad24dc0be
Karlo Grlić <karlogrlicweb@gmail.com>
2026-08-11 01:49:57 +0200

    Add: duplicate-integer - submission-9

---
+++ Data Structures & Algorithms/duplicate-integer/submission-9.rs
@@ -0,0 +1,14 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn has_duplicate(nums: Vec<i32>) -> bool {
+        let mut occurances_map = HashMap::new();
+        for elem in nums {
+            if occurances_map.contains_key(&elem) {
+                return true;
+            }
+            occurances_map.insert(elem, true);
+        }
+        return false;
+    }
+}
