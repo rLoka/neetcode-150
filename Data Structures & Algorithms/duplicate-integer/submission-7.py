e9b1c5a9271b8c6783c36bd4818a6ee1648c78ee
Karlo Grlić <karlogrlicweb@gmail.com>
2026-08-11 01:37:04 +0200

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.py
@@ -0,0 +1,22 @@
+# So, basically, simplest way is to use set() func which returns a strict set from array
+# Complexity of this solution is O(1), meaning there is no explicit iteration on array items
+# There could be implicit python iterations
+"""
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        if len(set(nums)) == len(nums):
+            return False
+        return True
+"""        
+# Since this is a built in function, let's try to implement solution ground up
+# As suggested by hint, we shall try to use HashMap -> a dictionary that memorizes how 
+# many times have we seen certain number - uses only one pass - O(n)
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        occurance_map = {}
+        for elem in nums:
+            if elem in occurance_map:
+                return True
+            else:
+                occurance_map[elem] = True
+        return False
